1dde5398499eb561b016473331fd1a9b43964e58
Jsalvar124 <jsalvar124@gmail.com>
2023-03-28 02:25:59 -0500

    se añade cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var authorsRouter = require('./routes/authors');
 var quotesRouter = require('./routes/quotes');
 
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
